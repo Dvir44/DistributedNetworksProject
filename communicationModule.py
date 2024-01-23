d029b9c8618d69383383aeb4806a5db7d25e35a2
Dvir44 <dvi4408@gmail.com>
2024-01-23 23:15:47 +0200

    Starting Format
    The first format for the modules for the project

---
+++ communicationModule.py
@@ -0,0 +1,12 @@
+import numpy
+
+class Communication:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
---
+++ translateModule.py
@@ -0,0 +1,12 @@
+import numpy
+
+class Translator:
+    def __init__(self):
+        pass
+
+
+def main():
+    pass
+
+if __name__=="__main__":
+    main()
