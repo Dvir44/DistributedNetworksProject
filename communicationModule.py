bb3c96e2a533d96102b8d38ed013ee8c460d2357
Daniel <danieli14072002@gmail.com>
2024-01-27 18:50:06 +0200

    modified:   communicationModule.py

--- communicationModule.py
+++ communicationModule.py
@@ -1,8 +1,20 @@
+import string
 import numpy
+
+from initializationModule import Computer
+
+
 
 class Communication:
     def __init__(self):
         pass
+    
+    def send_message(self, source: Computer, message: str, destination: list, parameters: dict):
+        pass
+    
+    def receive_message(self, source: Computer, parameters: dict):
+        if (not source.queue.empty):
+            pass
 
 
 def main():
--- initializationModule.py
+++ initializationModule.py
@@ -1,7 +1,17 @@
 import numpy
+import random
+from queue import PriorityQueue
+
+class Computer:
+    def __init__(self, parameters : dict):
+        self.queue= PriorityQueue()
+        self.id= parameters.get('id')
+        pass
+        
+
 
 class Initialization:
-    def __init__(self):
+    def __init__(self, computers: list, edges: dict, parameters: dict):
         pass
 
 
@@ -10,3 +20,7 @@ def main():
 
 if __name__=="__main__":
     main()
+    
+
+#computer_parameters = {'id': 5, 'other_attribute': 'some_value'}
+#computer = Computer(parameters = computer_parameters)
