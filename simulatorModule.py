70245495f23ad3e288ab98cd612ae3f08f7b64a9
Daniel <danieli14072002@gmail.com>
2024-08-16 13:19:30 +0300

    split simulator module into 2 separate functions

--- simulatorModule.py
+++ simulatorModule.py
@@ -17,28 +17,28 @@ NETWORK_VARIABLES = 'network_variables.json'
 OUTPUT_FILE = './output.txt'
 
 def load_network_variables():
-        """Load default variables from the JSON file."""
+        '''Load default variables from the JSON file.'''
         try:
             with open(NETWORK_VARIABLES, 'r') as f:
                 return json.load(f)
         except (FileNotFoundError, json.JSONDecodeError):
             return {}
 
-def main():
-    sys.stdout = open(OUTPUT_FILE, "w") # change default output
-    
+def initializeSimulator():
+    '''Initializes the simulator by creating a network and communication instances and loading the network_variables dictionary.''' 
     network_variables = load_network_variables()
-    data = MainMenu.menu(network_variables)
-    start_time = time.time()
+    MainMenu.menu(network_variables)
+    
     network= initializationModule.Initialization(network_variables)
     if network.logging_type!="Short":
         print(network)
-    
-    # with open(NETWORK_VARIABLES, 'r') as f:
-    #     data = json.load(f)
 
     comm = communication.Communication(network)
-
+    return network, comm, network_variables
+ 
+ 
+def runSimulator(network: initializationModule.Initialization, comm:communication.Communication, network_variables: dict, start_time):
+    '''Runs the simulator according to user provided network configuration and algorithm'''
     if network_variables['Display'] == "Graph":
         app = QApplication(sys.argv)
         graphVisualization.visualize_network(network, comm)
@@ -47,13 +47,15 @@ def main():
         thread.join()
         print("--- %s seconds ---" % (time.time() - start_time))
         sys.exit(app.exec_())
-        
     else:
         runModule.initiateRun(network, comm)
         print("--- %s seconds ---" % (time.time() - start_time))
 
-    
 
- 
+
 if __name__=="__main__":
-    main()
+    sys.stdout = open(OUTPUT_FILE, "w") # change default output
+    network, comm, network_variables = initializeSimulator()
+    
+    start_time = time.time()
+    runSimulator(network, comm, network_variables, start_time)
